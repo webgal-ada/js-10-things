1afc796021102b1d71aeaa57f80fcc32d9958525
Aurora <ada10016@yahoo.com>
2022-04-07 14:11:49 -0400

    first commit

--- 1-inheritance/example.js
+++ 1-inheritance/example.js
@@ -1,7 +1,9 @@
+//this was fork over from following Sina
+
 // Inheritance using Classes
 class Person {
   talk() {
-    return 'Talking';
+    return "Talking";
   }
 }
 
@@ -12,57 +14,52 @@ you.talk(); // Talking
 
 // To update the function for both instances you only have to do it once:
 Person.prototype.talk = function () {
-  return 'New and improved Talking';
-}; 
-
+  return "New and improved Talking";
+};
 
 // -----------------------------------------
 // Inheritance using a Constructor Function
-function Person() {};
+function Person() {}
 Person.prototype.talk = function () {
-  return 'Talking';
-}
+  return "Talking";
+};
 
 const me = new Person();
 const you = new Person();
 me.talk(); // Talking
 you.talk(); // Talking
 
-
 // -----------------------------------------
 // Inheritance using pure objects with Object.create
 const person = {
   talk() {
-    return 'Talking';
-  }
-}
+    return "Talking";
+  },
+};
 const me = Object.create(person);
 me.talk(); // Talking
 
-
 // -----------------------------------------
 // Inheritance using pure objects with Object.setPrototypeOf
-const person = {}
-person.__proto__.talk = function (){
-  return 'Talking';
-}
+const person = {};
+person.__proto__.talk = function () {
+  return "Talking";
+};
 const me = {};
 Object.setPrototypeOf(me, person);
 me.talk(); // Talking
-
-
 
 // -----------------------------------------
 // Extending a Class using 'extends'
 class Person {
   talk() {
-    return 'talking';
+    return "talking";
   }
 }
 
 class SuperHuman extends Person {
   fly() {
-    return 'flying';
+    return "flying";
   }
 }
 const me = new Person();
@@ -72,4 +69,3 @@ console.log(me.fly); // fly does NOT exists
 const you = new SuperHuman();
 console.log(you.fly); // fly exists
 console.log(you.talk); // talk also exists!
-
